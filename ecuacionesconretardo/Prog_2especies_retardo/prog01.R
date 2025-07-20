a92692886cef93eb844c9e7300d8fefced08f08a
matecom57 <matecom57@gmail.com>
2025-07-20 02:29:56 -0600

    Test comité

--- ecuacionesconretardo/Prog_2especies_retardo/prog01.R
+++ ecuacionesconretardo/Prog_2especies_retardo/prog01.R
@@ -9,64 +9,71 @@ t = seq(t1, t2, length.out = np)
 
 del = t[2] - t[1]
 
-p = matrix(rep(1,2*np), ncol=2)
+u = matrix(rep(0,2*np), ncol=2)
 
 numiter = 10
 
 nom = 'fig_'
 
 for (k in 1:numiter){
-  print(file)
-x = matrix(rep(0,2*np), ncol=2)
+  print('-----------------------------------------------')
+  x = matrix(rep(0,2*np), ncol=2)
 
-x1 = c(1,1)
-x[1,] = x1
+  x1 = c(1,1)
+  x[1,] = x1
 
-for (i in 2:np){
-  mat1 = matrix(c(1, x1[2], -x1[1], -1), ncol=2)
-  mat2 = matrix(c(p[i-1,1]*x1[1], 0, 0, p[i-1,2]*x1[2]), ncol=2)
-  x2 = x1 + del * (mat1 %*% x1 - .4*mat2 %*% x1)
-  x[i,] = x2
-  x1 = x2
-}
+  for (i in 2:np){
+    uu = u[i-1,]
+    mat1 = matrix(c(1, x1[2], -x1[1], -1), ncol=2)
+    mat2 = matrix(c(uu[1], 0, 0, uu[2]), ncol=2)
+    x2 = x1 + del * (mat1 %*% x1 - .4*(mat2 %*% x1))
+    x[i,] = x2
+    x1 = x2
+  }
 
-p2 =  c(.7, .5)
-p[np,] = p2
 
-for (i in (np-1):1){
-  mat1 = matrix(c(1, -x[i,1], x[i,2], 1), ncol = 2)
-  mat2 = matrix(c(x[i, 2]-.4*p2[1]*x[i,1], 0, 0, x[i, 1]-.4*p2[2]*x[i,2]), ncol=2)
-  p1 = p2 - del* (x[i,] + mat1 %*% p2 + mat2 %*% p2)
-  p[i,] = p1
-  p2 = p1
-}
+  p = matrix(rep(0,2*np), ncol=2)
 
-u = matrix(rep(0,2*np), ncol=2)
+  p2 =  c(.7, .5)
+  p[np,] = p2
 
-for (i in 1:np){
-  u[i,] = p[i,] * x[i,]
-}
+  for (i in (np-1):1){
+    uu = u[i+1,]
+    xx = x[i+1,]
+    mat1 = matrix(c(1, -xx[1], xx[2], 1), ncol = 2)
+    mat2 = matrix(c(xx[2]-.4*uu[1], 0, 0, xx[1]-.4*uu[2]), ncol=2)
+    p1 = p2 + del* (xx + mat1 %*% p2 + mat2 %*% p2)
+    p[i,] = p1
+    p2 = p1
+  }
 
-if (k < 10){
-   file = paste(nom,'0',k, '.jpeg', sep='')
-} else {
-   file = paste(nom,k, '.jpeg', sep='')
-}
+  u = matrix(rep(0,2*np), ncol=2)
 
-jpeg(file=file)
+  for (i in 1:np){
+    u[i,] = p[i,] * x[i,]
+  }
 
-par(mfrow=c(3,2))
 
-plot(x[,1], type='l')
+  if (k < 10){
+    file = paste(nom,'0',k, '.jpeg', sep='')
+  } else {
+    file = paste(nom,k, '.jpeg', sep='')
+  }
 
-plot(x[,2], type='l')
+  jpeg(file=file)
 
-plot(p[,1], type='l')
+  par(mfrow=c(3,2))
+
+  plot(x[,1], type='l')
+
+  plot(x[,2], type='l')
+
+  plot(p[,1], type='l')
   
-plot(p[,2], type='l')
+  plot(p[,2], type='l')
 
-plot(u[,1], type='l')
+  plot(u[,1], type='l')
  
-plot(u[,2], type='l')
-dev.off()
+  plot(u[,2], type='l')
+  dev.off()
 }
