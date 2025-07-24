6dbca297a33705d67bf2ca21a58847fb3060ed53
santosg572 <santosg572@gmail.com>
2025-07-24 09:49:41 -0600

    Test comité

--- antonieta_CUTE_Programas_ART_nov1221/SolucionXPU.R
+++ antonieta_CUTE_Programas_ART_nov1221/SolucionXPU.R
@@ -1,4 +1,5 @@
 SolucionXPU <- function(del=0, pp=0, wx1=0, wx2=0){
+   print('--------------------- SolucionXPU.R ----------------------')
    ss = dim(pp)
 
    n = ss[1]
@@ -10,43 +11,33 @@ SolucionXPU <- function(del=0, pp=0, wx1=0, wx2=0){
    dwx2 = wx2 *sqrt(del)*runif(n)
    
 
-   	x1 = c(.5, .7)
-   	xx[1,] = x1
-	p1 = pp[1,]
-	w1 = dwx1[1]
-	w2 = dwx2[1]
-	#u1 = Calu(x1, p1)
-	u = Calu1u2(x1, p1)
+   x1 = c(.5, .7)
+   xx[1,] = x1
+   p1 = pp[1,]
+   w1 = dwx1[1]
+   w2 = dwx2[1]
+   u = Calu1u2(x1, p1)
 	
-   	for (i in 2:n){
-   		A = matrix(c(1 - 0.4 * u[1], x1[2], -x1[1], -1 - 0.2*u[2]), ncol=2)
-		res = A %*% x1
-#		cat('w1=',w1,' w2=', w2,'\n')
-#		print('x1')
-#		print(x1)
-#		print('del*res')
-#		print(del*res)
-	  	x2 = x1 + del*res + c(w1, w2)
-#	  	cat('x2=', x2,'\n')
-	  	xx[i,] = x2
-	  	x1 = x2
-#	  	print(x1)
-		p1 = pp[i,]
-		w1 = dwx1[i]
-		w2 = dwx2[i]
-		#u1 = Calu(x1, p1)
-		u = Calu1u2(x1, p1)
+   for (i in 2:n){
+      A = matrix(c(1 - 0.4 * u[1], x1[2], -x1[1], -1 - 0.2*u[2]), ncol=2)
+      res = A %*% x1
+      x2 = x1 + del*res + c(w1, w2)
+      xx[i,] = x2
+      x1 = x2
+      p1 = pp[i,]
+      w1 = dwx1[i]
+      w2 = dwx2[i]
+      u = Calu1u2(x1, p1)
    }
    
    uu = matrix(rep(0,2*n), ncol=2)
    
    for (i in 1:n){
-   		pi = pp[i,]
-   		xi = xx[i,]
-        uu[i, ] = Calu1u2(xi,pi)
+      pi = pp[i,]
+      xi = xx[i,]
+      uu[i, ] = Calu1u2(xi,pi)
    }
-   
-  
+     
    res = list(xx=xx, uu=uu)
 }
 
