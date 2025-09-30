82d4fc2a5fda07949c28a1c843e1755c8acd293e
santosg572 <santosg572@gmail.com>
2025-09-30 12:03:30 -0600

    Test comité

---
+++ ecuaciones_2_retardos/cute_sep1025_ecuaciones/procesa.R
@@ -0,0 +1,27 @@
+del1 = .001
+np = 60 / del1 +
+
+print(n)
+
+ni =1
+beta = 1
+delta = 1
+w=1
+epsi=1
+k = 1
+r=0
+s=0
+
+x1 = rep(0,np)
+x2 = rep(0,np)
+x3 = rep(0,np)
+
+u10 = 0
+u20 = 0
+u3 = 0
+
+A1 = 1
+A2 = 2
+A3 = 3
+
+
