6ff3f8bee182de7c119368138b2497845627efa9
matecom57 <matecom57@gmail.com>
2025-10-12 13:52:47 -0600

    Test comité

--- creaHTML.py
+++ creaHTML.py
@@ -1,3 +1,5 @@
+import sys
+
 d1='''
 <!DOCTYPE html>
 <html>
@@ -12,22 +14,25 @@ d2 = '''
 </html>
 '''
 
-filin = open('dir.txt', 'r')
+file = sys.argv[1]
+filin = open(file+'.txt', 'r')
 datos = filin.readlines()
 filin.close()
 
 print(datos)
 
-filon = open('dir.html', 'w')
+filon = open(file+'.html', 'w')
 filon.write(d1)
 
-k=1
+pat = datos[0]
+pat = pat.replace('\n','')
+
+datos = datos[1:]
 
 for ss in datos:
   ss = ss.replace('\n', '')
-  ss1 = '<p> <a href="' + ss + '">' + 'file' + str(k) + '</a> </p>\n'
+  ss1 = '<p> <a href="' + pat + '/' + ss + '">' + ss + '</a> </p>\n'
   filon.write(ss1)
-  k = k+1
 
 filon.write(d2)
 filon.close()
