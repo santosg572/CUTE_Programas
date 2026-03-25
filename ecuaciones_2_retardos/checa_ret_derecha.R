583487fe8ae1e67f1f3ac87f848358071a5c27c1
santosg572 <santosg572@gmail.com>
2026-03-25 15:10:21 -0600

    Test comité

--- ecuaciones_2_retardos/checa_ret_derecha.R
+++ ecuaciones_2_retardos/checa_ret_derecha.R
@@ -12,7 +12,9 @@ x2 = 1 - tao^2/2 + t2 + t2^2/2
 t = c(t1, t2)
 x = c(x1, x2)
 
+print('tttt')
 print(t)
+print('xxxxx')
 print(x)
 
 to = seq(0,tao,del)
@@ -22,8 +24,25 @@ yi = rep(1, np)
 t = seq(0, 2*del, del)
 nT = length(t)
 
-y = rep(0,n)
+y = rep(0,nT)
 
 y1 = yi[np]
+y[1] = yi[np]
 
+for (i in 2:np){
+  y2 = y1 + del * yi[i-1]
+  y1 = y2
+  y[i] = y1
+}
 
+print(y)
+
+k = np+1
+
+for (i in (np+1):nT){
+  y2 = y1 + del * yi[i-1]
+  y1 = y2
+  y[i] = y1
+}
+
+print(y)
