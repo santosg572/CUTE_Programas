493c50e461b5c5a0bca4da0551b5770adff8b3e0
santosg572 <santosg572@gmail.com>
2026-06-23 14:07:08 -0600

    Test comité

--- pp/p1_hacia_atras.R
+++ pp/p1_hacia_atras.R
@@ -1,24 +1,30 @@
-# y' = t^2 + 2 - y(t-1)
+# y' = -t^2 - 2 + y(t+1)
 # solucion:
 # f(t) = t^2 + 1
 #
 
-del1 = .001
+del1 = .1
 n = 1/del1
 
 nn = 10*n-(10-1)
 
 y = rep(0, nn)
-y[(nn-n+1):nn] = 100-1
+y[(nn-n+1):nn] = 100+1
 
+print(y)
+print(n)
+print(nn)
 y2 = y[nn-n+1]
+print(y2)
 for (i in (nn-n):1){
-   y1 = y2 - del1 * ((i*del1)^2 + 2 - y[i+n])
+   y1 = y2 - del1 * (-1*((i+1)*del1)^2 - 2 + y[i+n])
    y2 = y1
-   y[i] = y2
+   y[i] = y1
 }
+print(y)
 y = y[1:(nn-n)]
 ny = length(y)
+print(ny)
 tt =  del1 * 0:(ny-1)
 
 print(length(y))
@@ -27,6 +33,6 @@ print(length(tt))
 
 plot(tt, y, type='l')
 
-#yt = tt^2 + 1
+yt = tt^2 + 1
 
 #points(tt, yt+2, type='l', col='red')
